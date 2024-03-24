63b55c3ab4e3a969ea6350773e77c6fd7e5eec7e
Abiksoumya <soumyamohanty540@gmail.com>
2024-03-24 09:49:56 +0530

    ssl integrate

--- fatafat-backend/src/server.ts
+++ fatafat-backend/src/server.ts
@@ -9,6 +9,9 @@ import { connect } from "mongoose";
 // import { DB, PORT, app } from "./app";
 import { config } from "dotenv";
 import result from "./routes/result.router";
+import https from "https";
+import path from "path";
+import fs from "fs";
 config();
 const app = express();
 
@@ -25,13 +28,19 @@ app.use("/transaction", transRouter);
 const PORT = 5000;
 const DB = 'mongodb+srv://soumyamohanty540:w0NXkYtEv2MTf8L2@cluster0.qlof1tt.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'
 
+const secureServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname,"cert", "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname,"cert", "cert.pem")),
+},
+app
+)
 connect(DB as string)
   .then((connection) => {
     console.log(
       `🔑 Authentication database successfully running on ${connection.connection.host}`
     );
     // handlerUserCreateEvent();
-    app.listen(PORT, () => {
+    secureServer.listen(PORT, () => {
       console.log(`🔑 Server is running on port ${PORT}`);
     });
   })
